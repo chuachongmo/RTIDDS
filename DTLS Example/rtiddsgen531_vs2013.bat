e14724822e7ec067afc0eb53ee446e999374eedf
Calvin Chua <chuachongmo@gmail.com>
2023-03-06 16:25:50 +0800

    Add files via upload

---
+++ DTLS Example/rtiddsgen531_vs2013.bat
@@ -0,0 +1,10 @@
+set "current_dir=%cd%"
+
+IF NOT EXIST HelloWorld (
+echo Creating HelloWorld Directory
+md HelloWorld
+) ELSE (
+echo HelloWorld Already exist
+)
+
+rtiddsgen "%current_dir%\HelloWorld.idl" -ppDisable -d "%current_dir%\HelloWorld" -language C++11 -create typefiles -create examplefiles -create makefiles -platform i86Win32VS2013 -stl -sharedLib 
